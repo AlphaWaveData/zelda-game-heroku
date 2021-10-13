0fe4659de3b38f906977764f261be7620558dc9f
Hugh <hdonnelly6@gmail.com>
2021-10-13 00:06:22 -0400

    updated project to roll way way back to Kyle version using Phaser 3.24.1

--- server/authoritative_server/js/game.js
+++ server/authoritative_server/js/game.js
@@ -169,12 +169,8 @@ class Room1 extends Phaser.Scene {
                 });
                 socket.on('disconnect', function() {
                     console.log('a user disconnected '+socket.id);
-                    // var used = process.memoryUsage().heapUsed / 1024 /1024;
-                    // console.log(`Usage: ${Math.round(used * 100) / 100} MB`);
-                    // console.log(process.memoryUsage());
                     _this.players[socket.id].body.destroy();
                     _this.players[socket.id].weapon.destroy();
-                    // _this.game.destroy(true);
                     delete _this.players[socket.id];
                     socket.removeAllListeners();
                     socket.disconnect();
@@ -184,11 +180,6 @@ class Room1 extends Phaser.Scene {
                     if(Object.keys(_this.players).length == 0){
                         console.log("server resetting");
                         _this.scene.restart();
-                        // _this.game.destroy(true);
-                        // var used = process.memoryUsage().heapUsed / 1024 /1024;
-                        // console.log(`Usage: ${Math.round(used * 100) / 100} MB`);
-                        console.log("Manual gc");
-                        // gc();
                     }
                 });
             }
